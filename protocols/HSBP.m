86d13b10242a5092aedecebc6ca47a0ae4464fcb
abdalla-agamy <abdalla.agamy@gmail.com>
2026-08-17 00:54:54 +0300

    Instrument HSBP leader and follower execution times

--- protocols/HSBP.m
+++ protocols/HSBP.m
@@ -26,20 +26,27 @@ classdef HSBP < Protocol
 
             activeUAVs = cluster.getActiveUAVs();
 
+            leaderTimer = tic;
             newGroupKey = obj.engine.generateGroupKey(activeUAVs);
+            leaderTime = toc(leaderTimer);
 
             cluster.groupKey = newGroupKey;
 
+            followerTimer = tic;
             for i = 1:numel(activeUAVs)
 
                 activeUAVs(i).groupKey = newGroupKey;
                 activeUAVs(i).keySynced = true;
 
             end
+            followerTime = toc(followerTimer);
 
             result = obj.createRekeyResult( ...
                 activeUAVs, ...
-                clusterID)
+                clusterID);
+
+            result.leaderTime = leaderTime;
+            result.followerTime = followerTime;
 
             obj.addCost(cluster.count());
 
@@ -68,19 +75,27 @@ classdef HSBP < Protocol
 
             if ~isempty(activeUAVs)
 
+                leaderTimer = tic;
                 newGroupKey = obj.engine.generateGroupKey(activeUAVs);
+                leaderTime = toc(leaderTimer);
 
                 cluster.groupKey = newGroupKey;
 
+                followerTimer = tic;
                 for i = 1:numel(activeUAVs)
 
                     activeUAVs(i).groupKey = newGroupKey;
                     activeUAVs(i).keySynced = true;
 
                 end
+                followerTime = toc(followerTimer);
+
                 result = obj.createRekeyResult( ...
                     activeUAVs, ...
                     clusterID);
+
+                result.leaderTime = leaderTime;
+                result.followerTime = followerTime;
 
             else
 
@@ -116,20 +131,27 @@ classdef HSBP < Protocol
 
             if ~isempty(activeUAVs)
 
+                leaderTimer = tic;
                 newGroupKey = obj.engine.generateGroupKey(activeUAVs);
+                leaderTime = toc(leaderTimer);
 
                 cluster.groupKey = newGroupKey;
 
+                followerTimer = tic;
                 for i = 1:numel(activeUAVs)
 
                     activeUAVs(i).groupKey = newGroupKey;
                     activeUAVs(i).keySynced = true;
 
                 end
+                followerTime = toc(followerTimer);
 
                 result = obj.createRekeyResult( ...
                     activeUAVs, ...
                     clusterID);
+
+                result.leaderTime = leaderTime;
+                result.followerTime = followerTime;
 
             else
 
